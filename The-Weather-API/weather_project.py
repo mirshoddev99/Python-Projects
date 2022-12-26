8eb2e88610bf14f9c9b94af482c111d3976c2ddb
mirshod <oripovmirshod9@gmail.com>
2022-12-26 23:04:09 +0900

    api commit

--- The-Weather-API/weather_project.py
+++ The-Weather-API/weather_project.py
@@ -1,4 +1,5 @@
 import requests
+import pprint
 
 API_KEY = "dd50f5b11643407190761005222412"
 BASE_URL = "http://api.weatherapi.com/v1/current.json?key="
@@ -8,31 +9,30 @@ req_url = f"{BASE_URL}{API_KEY}&q={country_city}&aqi=yes"
 response = requests.get(req_url)
 
 my_data = response.json()
-print(my_data)
+pprint.pprint(my_data)
+
+
 # print(my_data.keys())
 
 
 def print_data():
-
     # checking status code and type data of response
     if response.status_code == 200 and response.headers['Content-Type'] == "application/json":
-        print("Success!\n")
+        print(f"\t\t\n........The Weather forecast........")
+
+        info = f"""
+            County: {my_data['location']['country']}
+            City: {my_data['location']['name']}
+            Temperature: {my_data['current']['temp_c']} celsius
+            Temperature: {my_data['current']['temp_f']} fahrenheit
+            The weather condition: {my_data['current']['condition']['text']}
+            Humidity: {my_data['current']['humidity']}
+            Cloud: {my_data['current']['cloud']}
+            """
+        print(info)
+
     else:
         print("An error occurred!\n")
-
-    print(f"\t\t\n........The Weather forecast........")
-
-    print(f"""
-
-        County: {my_data['location']['country']}
-        City: {my_data['location']['name']}
-        Temperature: {my_data['current']['temp_c']} celsius
-        The weather condition: {my_data['current']['condition']['text']}
-        Humidity: {my_data['current']['humidity']}
-        Cloud: {my_data['current']['cloud']}
-
-    """
-          )
 
 
 print_data()
@@ -50,5 +50,4 @@ def download_img():
         f.write(img.content)
         print(f"The image of the weather of {city} was downloaded successfully!")
 
-
-download_img()
+# download_img()
